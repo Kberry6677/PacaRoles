2cefc11aa21714261db743cef33b1b61e866b401
kilroy6677 <kilroy6677@gmail.com>
2014-10-03 16:41:20 -0700

    Program in C#
    A program I designed and coded to simplify a daily/weekly task my team
    was given.  The program saves the team a couple hours of work time.

---
+++ PacaRoles/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace PacaRoles
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.txtFilename = new System.Windows.Forms.TextBox();
+            this.btnGetFile = new System.Windows.Forms.Button();
+            this.dlgFilePick = new System.Windows.Forms.OpenFileDialog();
+            this.txtResults = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnExport = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(197, 37);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(159, 23);
+            this.btnExit.TabIndex = 8;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnRun
+            // 
+            this.btnRun.Location = new System.Drawing.Point(16, 37);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(159, 23);
+            this.btnRun.TabIndex = 7;
+            this.btnRun.Text = "Run";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // txtFilename
+            // 
+            this.txtFilename.Location = new System.Drawing.Point(97, 11);
+            this.txtFilename.Name = "txtFilename";
+            this.txtFilename.ReadOnly = true;
+            this.txtFilename.Size = new System.Drawing.Size(259, 20);
+            this.txtFilename.TabIndex = 6;
+            // 
+            // btnGetFile
+            // 
+            this.btnGetFile.Location = new System.Drawing.Point(16, 8);
+            this.btnGetFile.Name = "btnGetFile";
+            this.btnGetFile.Size = new System.Drawing.Size(75, 23);
+            this.btnGetFile.TabIndex = 5;
+            this.btnGetFile.Text = "Get File";
+            this.btnGetFile.UseVisualStyleBackColor = true;
+            this.btnGetFile.Click += new System.EventHandler(this.btnGetFile_Click);
+            // 
+            // txtResults
+            // 
+            this.txtResults.Location = new System.Drawing.Point(16, 66);
+            this.txtResults.Multiline = true;
+            this.txtResults.Name = "txtResults";
+            this.txtResults.ReadOnly = true;
+            this.txtResults.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtResults.Size = new System.Drawing.Size(340, 289);
+            this.txtResults.TabIndex = 10;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btnExport
+            // 
+            this.btnExport.Location = new System.Drawing.Point(197, 361);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(158, 23);
+            this.btnExport.TabIndex = 11;
+            this.btnExport.Text = "Export to Word";
+            this.btnExport.UseVisualStyleBackColor = true;
+            this.btnExport.Click += new System.EventHandler(this.btnExport_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(367, 392);
+            this.Controls.Add(this.btnExport);
+            this.Controls.Add(this.txtResults);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnRun);
+            this.Controls.Add(this.txtFilename);
+            this.Controls.Add(this.btnGetFile);
+            this.Name = "Form1";
+            this.Text = "Get PACA Roles";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnRun;
+        private System.Windows.Forms.TextBox txtFilename;
+        private System.Windows.Forms.Button btnGetFile;
+        private System.Windows.Forms.OpenFileDialog dlgFilePick;
+        private System.Windows.Forms.TextBox txtResults;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnExport;
+    }
+}
+
---
+++ PacaRoles/Form1.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using System.Data.Services.Client;
+using PacaRoles.DataReference;
+using Microsoft.Office;
+using Word = Microsoft.Office.Interop.Word;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace PacaRoles
+{
+    public partial class Form1 : Form
+    {
+        string filename;
+        IdentityDataSource source;
+        bool paca = false;
+        int teamCount;
+        Excel.Application excelPaca;
+        Excel._Worksheet workSheet;
+
+        public Form1()
+        {
+            InitializeComponent();            
+        }
+
+        private void btnGetFile_Click(object sender, EventArgs e)
+        {
+            dlgFilePick.Title = "Open File";
+            dlgFilePick.InitialDirectory = @"C:\";
+            if(dlgFilePick.ShowDialog() == DialogResult.OK)
+            {
+                filename = dlgFilePick.FileName;
+                txtFilename.Text = filename;
+            }
+        }
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+            if (filename == null)
+            {
+                MessageBox.Show("Error!  You have not selected a file.");
+            }
+            else
+            {
+                runProgram();
+            }
+            MessageBox.Show("Program completed.");
+            workSheet.Columns[1].AutoFit();
+            workSheet.Columns[2].AutoFit();
+            DateTime excelDate = DateTime.Now;
+            string savePath = @"\\mlsfs\users\v-krberr\PACA_" + excelDate.ToString("MMddyy_HHmm") + ".xlsx";
+            workSheet.SaveAs(savePath, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing);
+            excelPaca.Visible = true;
+        }
+
+        private void runProgram()
+        {
+            createExcel();
+            teamCount = 1;
+            var names = File.ReadAllLines(filename).ToList();
+
+            foreach (var line in names)
+            {
+                teamCount++;
+                paca = false;
+                var parentName = line;                
+                getTeamChildren(parentName, parentName);
+                enterExcel(teamCount, paca, parentName);
+            }
+
+        }
+
+        private void getTeamChildren(string parentName, string cookieCrumbs)
+        {
+            var parentId = getTeamID(parentName);
+
+            if (parentId == null)
+            {
+                txtResults.AppendText("Parent Team " + parentName + " not found." + Environment.NewLine);
+                return;
+            }
+            var childTeams = from t in source.Teams
+                where t.ParentId == parentId
+                select t;
+
+            printParentChildren(cookieCrumbs, childTeams);
+        }
+
+        private void printParentChildren(string cookieCrumbs, IQueryable<Team> childTeams)
+        {
+            txtResults.AppendText(cookieCrumbs + Environment.NewLine);
+
+            foreach (var team in childTeams)
+            {
+                if (team.Category != "Standard")
+                {
+                    txtResults.AppendText(" - " + team.DisplayName + Environment.NewLine);
+                    var childId = getChildTeamId(team.DisplayName, team.ParentId);
+
+                    if (childId == null)
+                    {
+                        txtResults.AppendText("No child team found for " + cookieCrumbs);
+                        return;
+                    }
+                    var roles = from r in source.RoleAssignments
+                                where r.ParentId == childId
+                                select r;
+
+                    printChildRoles(roles);
+                }
+            }
+        }
+
+        private void printChildRoles(IQueryable<RoleAssignment> childTeam)
+        {
+            foreach(var role in childTeam)
+            {
+                if(role.DisplayName == null)
+                {
+                    return;
+                }
+                if(role.DisplayName == "MSAuth.Physical Asset Change Authorizations")
+                {
+                    paca = true;
+                    txtResults.AppendText("          Role - " + role.DisplayName + Environment.NewLine);
+                }
+            }
+        }
+
+        private string getTeamID(string teamName)
+        {
+            var rootTeam = from p in source.Teams
+                           where p.DisplayName == teamName && p.BoundaryNodeType == "BRH.Application"
+                           select p;
+            var count = 0;
+            string rootTeamId;
+
+            foreach (var item in rootTeam)
+                count++;
+
+            if(count == 0)
+            {
+                return null;
+            }
+            try
+            {
+                rootTeamId = rootTeam.FirstOrDefault().Id;
+            }
+            catch
+            {
+                rootTeamId = null;
+            }
+
+            return rootTeamId;
+        }
+
+        private string getChildTeamId(string teamName, string parentID)
+        {
+            var childTeam = from t in source.Teams
+                            where t.ParentId == parentID && t.DisplayName == teamName
+                            select t;
+                        
+            var childTeamId = childTeam.FirstOrDefault().Id;
+
+            return childTeamId;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var IdentityURL = new Uri("http://identity.api.msdial.com/Directory.svc");
+            source = new DataReference.IdentityDataSource(IdentityURL)
+            {
+                MergeOption = MergeOption.OverwriteChanges,
+                IgnoreMissingProperties = true,
+                IgnoreResourceNotFoundException = true,
+                Credentials = CredentialCache.DefaultNetworkCredentials
+            };
+
+            txtResults.AppendText("PACA Role Check:" + Environment.NewLine);
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnExport_Click(object sender, EventArgs e)
+        {           
+            String txt = txtResults.Text;
+            var date = DateTime.Now;
+            TextWriter sw = new StreamWriter(Path.GetDirectoryName(filename) + @"\output_" + date.ToString("MMddyy_HHmm") + ".doc");
+            sw.WriteLine(txt);
+            sw.Close();
+
+            if(File.Exists(Path.GetDirectoryName(filename) + @"\output_" + date.ToString("MMddyy_HHmm") + ".doc"))
+            {
+                MessageBox.Show("Information exported correctly");
+            }
+            else
+            {
+                MessageBox.Show("Export failed.");
+            }
+        }
+
+        private void createExcel()
+        {
+            excelPaca = new Excel.Application();
+            excelPaca.Workbooks.Add();
+
+            workSheet = (Excel.Worksheet)excelPaca.ActiveSheet;
+
+            workSheet.Cells[1, "A"] = "Property Dimension/Application";
+            workSheet.Cells[1, "B"] = "PACA";
+
+        }
+        
+        private void enterExcel(int row, bool role, string name)
+        {
+            string roleDes;
+
+            if(role)
+            {
+                roleDes = "Y";
+            }
+            else
+            {
+                roleDes = "N";
+            }
+
+            workSheet.Cells[row, "A"] = name;
+            workSheet.Cells[row, "B"] = roleDes;
+        }
+    }
+}
